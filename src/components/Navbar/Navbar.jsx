49ab6387cd774e333645098977cdcf0aeeab8b71
turboBarbitura <turbobarbitura@mail.ru>
2021-11-14 22:48:23 +0700

    fixed bages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import s from './Navbar.modules.css'
+import s from './Navbar.module.css'
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <div>
+      <div className={`${s.item} ${s.active}`}>
         <a>Profile</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>Messages</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>News</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>Music</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>Settings</a>
       </div>
     </nav>
