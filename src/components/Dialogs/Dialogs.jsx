60e2032e8ecb05ab545c6dbd00fba0dd89453240
turboBarbitura <turbobarbitura@mail.ru>
2021-11-14 22:15:55 +0700

    add dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Dialogs.module.css'
+
+const Dialogs = () => {
+    return (
+        <div className={s.dialog}>
+            dialogs
+        </div>
+    );
+};
+
+export default Dialogs;
