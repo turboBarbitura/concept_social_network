81fa702079fa7282e72c8104022c4f02a573b478
turboBarbitura <turbobarbitura@mail.ru>
2021-11-16 17:05:39 +0700

    add router dom v6 integrated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import s from './Navbar.module.css'
-import {Link} from "react-router-dom";
+
+import {NavLink} from "react-router-dom";
+
 
 const Navbar = () => {
-  return (
-    <nav className={s.nav}>
-      <div className={`${s.item} ${s.active}`}>
-        <Link to='/profile'>Profile</Link>
-      </div>
-      <div className={s.item}>
-        <Link to='/dialogs'>Messages</Link>
-      </div>
-      <div className={s.item}>
-        <a>News</a>
-      </div>
-      <div className={s.item}>
-        <a>Music</a>
-      </div>
-      <div className={s.item}>
-        <a>Settings</a>
-      </div>
-    </nav>
-  )
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to='/profile'>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink className={({ isActive }) => isActive ? s.activeLink : ""}
+                to='/dialogs'>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <a>News</a>
+            </div>
+            <div className={s.item}>
+                <a>Music</a>
+            </div>
+            <div className={s.item}>
+                <a>Settings</a>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
