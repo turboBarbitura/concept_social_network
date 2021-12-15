802b1e385fb3d7b65fe2bc5c3820e55b2cd5dd68
turboBarbitura <turbobarbitura@mail.ru>
2021-12-15 17:06:44 +0700

    split reducer

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
+
 
 
 const MyPosts = (props) => {
